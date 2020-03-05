06009946b0da03b427ad8429fa7c4bdc2babd435
luigi <luigiteteu@outlook.com>
2020-03-05 17:10:07 -0300

    muh

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const moment = require('moment');
+const dayjs = require('dayjs')
 const app = express();
 const mysql = require('mysql');
 const connection = mysql.createConnection({
@@ -32,27 +33,24 @@ app.get('/eventos', function (req, res) {
         function (error, results, fields) {
             if (error)
                 res.json;
-            else
+            else {
                 res.json(results);
-            console.log('executou /eventos');
-        }
+                results.forEach(element => {
+                    var data = element['date_hora']
+                    element['date_hora'] = dayjs(data).format('MMMM D, YYYY')
+                    console.log(element['date_hora'])
+                });
+                console.log('executou /eventos')
+            }
 
-    )
+        })
 });
 
-app.post('/add_eventoo', function (req, res) {
-    var sql = "insert into evento(nome, date_hora) values('da certo', '2020-12-25 11:11:11')"
-    connection.query(function (error, results, fields) {
-        if (error)
-            res.json;
-        else
-            res.json(results);
-        console.log('executou /add_eventoo');
-    });
-});
 
-app.post('/add_evento', function (_nome, _data, req, res) {
+app.post('/add_evento', function (req, res) {
     console.log(req.body)
+    req.body.name
+    req.body.data
     var nomeEvento = _nome
     var dataEvento = moment(_data).format("ll")
     connection.query(`insert into evento(nome, date_hora) values('${nomeEvento}', '${dataEvento}')`, function (error, results, fields) {
